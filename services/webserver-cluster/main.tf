1c3f231b956c116db4d131f9e3f62199295ff235
Alexander <alexander@adscale.com>
2025-08-08 11:02:13 +0300

    autoscaling

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -146,7 +146,7 @@ resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
     count = var.enable_autoscaling ? 1 : 0
 
     scheduled_action_name = "${var.claster_name}-scale-out-during-business-hours"
-    autoscaling_group_name = module.webserver_cluster_prod.asg_name
+    autoscaling_group_name = aws_autoscaling_group.this.name
     min_size               = 2
     max_size               = 10
     desired_capacity       = 10
@@ -157,7 +157,7 @@ resource "aws_autoscaling_schedule" "scale_in_at_night" {
     count = var.enable_autoscaling ? 1 : 0
 
     scheduled_action_name = "${var.claster_name}-scale-out-during-business-hours"
-    autoscaling_group_name = module.webserver_cluster_prod.asg_name
+    autoscaling_group_name = aws_autoscaling_group.this.name
     min_size               = 2
     max_size               = 10
     desired_capacity       = 2
